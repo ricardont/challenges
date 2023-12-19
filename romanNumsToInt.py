043543df6edce40a3d9be7e3e0624bc4ef703594
Ricky <ricky@ricky.io>
2023-12-18 23:15:00 -0600

    add comments to romans to num

--- romanNumsToInt.py
+++ romanNumsToInt.py
@@ -1,6 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romanLttr = {
+        # dictionary definition is a must {"I":1} - 
+        rlv = {
             "I":1,
             "V":5,
             "X":10,
@@ -8,33 +9,26 @@ class Solution:
             "C":100,
             "D":500,
             "M":1000,
-            "":0
         }
-        res = 0     
-        for i in range(len(s)-1):  
-            if romanLttr[s[i]] < romanLttr[s[i+1]]:
-                res -= romanLttr[s[i]] 
-            else:
-                res += romanLttr[s[i]]            
-        return res+romanLttr[s[-1]]
-    
+        res = 0
+        # loop through all string letters from left to righ - but the last element to avoid array limit errors
+        for i in range(len(s)-1):
+        #   get letter numeric value = > lnv
+        #   if c urrent lnv is lower than succesor lnv  substracr it to the final result else  add it up to the final result 
+            if rlv[s[i]] < rlv[s[i+1]]:
+                res -= rlv[s[i]] 
+            else:     
+                res += rlv[s[i]] 
+        # add up the last element left (excluded from previous loop)
+        res += rlv[s[-1]]
+        return res
 
-print("DCXXI equals 621 => " + str(Solution().romanToInt("DCXXI")))
-print("VIII equals 8 => " + str(Solution().romanToInt("VIII")))
-print("III equals 3 => " + str(Solution().romanToInt("III")))
-print("X equals 10 => " + str(Solution().romanToInt("X")))
-print("IX equals 9 => " + str(Solution().romanToInt("IX")))
-print("IV equals 4 => " + str(Solution().romanToInt("IV")))
-print("LVIII equals 58 => " + str(Solution().romanToInt("LVIII")))
-print("MCMXCIV equals 1994 => " + str(Solution().romanToInt("MCMXCIV")))
-# s = "IVX"
-# print(s[len(s)-1])
-# print(len(s)-1)
-
-# dictionary definition is a must {"I":1} - 
-# Concerns 
-#   validate only permited characters   - 
-#   validate within range [1, 3999], else non 
-# loop through all string letters from left to righ -
-# save letter predecesro, letter, letter succesor, 
-# get equivalence single letter number
+#test cases
+print("IX " + str(Solution().romanToInt("IX")) + " => " + str(Solution().romanToInt("IX") == 9))
+print("MCMXCIV " + str(Solution().romanToInt("MCMXCIV")) + " => " + str(Solution().romanToInt("MCMXCIV") == 1994))
+print("IV " + str(Solution().romanToInt("IV")) + " => " + str(Solution().romanToInt("IV") == 4))
+print("LVIII " + str(Solution().romanToInt("LVIII")) + " => " + str(Solution().romanToInt("LVIII") == 58))
+print("DCXXI " + str(Solution().romanToInt("DCXXI")) + " => " + str(Solution().romanToInt("DCXXI") == 621))
+print("VIII " + str(Solution().romanToInt("VIII")) + " => " + str(Solution().romanToInt("VIII") == 8))
+print("III " + str(Solution().romanToInt("III")) + " => " + str(Solution().romanToInt("III") == 3))
+print("X " + str(Solution().romanToInt("X")) + " => " + str(Solution().romanToInt("X") == 10))
